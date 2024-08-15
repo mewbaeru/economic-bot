03933844194b15deae3acdc4fe095112290ef377
mewbaeru <klyukina.anzhelika@mail.ru>
2024-08-15 03:17:25 +0300

    feat(Shop.py): add cog to shops with server roles (personal and additonal)

---
+++ cogs/Shop.py
@@ -0,0 +1,34 @@
+from disnake.ext import commands
+
+from database.requests import get_shop_roles
+from modules import *
+
+import math
+
+guild_id = Utils.get_guild_id()
+
+# get cost additional role
+try: 
+    additional_role_id, cost_additional_role = Utils.get_cost_additional_role()
+except Exception:
+    logger.error('failed to load settings')
+
+class Shop(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.slash_command(name='shop', description='Магазин личных ролей', guild_ids=[guild_id])
+    async def shop(self, ctx):
+        roles = await get_shop_roles()
+        total_pages = math.ceil(len(roles)/5)
+        page = 1
+        
+        if roles:
+            embed = set_shop(ctx, roles, page, total_pages)
+            await ctx.response.send_message(embed=embed, view=ShopView(ctx, page, roles, total_pages, additional_role_id, cost_additional_role))
+        else: 
+            embed = set_empty_shop(ctx)
+            await ctx.response.send_message(embed=embed)
+
+def setup(client):
+    client.add_cog(Shop(client))
